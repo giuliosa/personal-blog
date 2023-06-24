ee62ad4ba9752c651bc674c8797338a291c41de0
Giulio Sá <giuliocaetano@yahoo.com.br>
2023-06-23 21:29:40 -0300

    Adding posts structure

--- components/home/home.tsx
+++ components/home/home.tsx
@@ -1,11 +1,18 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function HomePage() {
     return (
-        <section>     
+        <section className="text-center">     
             <Link href="/posts/first-post">
                 Posts    
             </Link>       
+            <p>Lvl 30 🎬💻🎨🎧📚🎮🏋️🍔🍩
+            Casado com a melhor esposa de todas, pai de três fofuras sem igual</p>
+            <p>
+                (This is a sample website - you’ll be building a site like this on{' '}
+                <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+            </p>
         </section>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,42 @@
 import Head from 'next/head'
-import Layout from '../components/layout'
+import Layout, { siteTitle } from '../components/layout'
 import HomePage from '../components/home/home'
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
-    <Layout>
+    <Layout home>
       <Head>
+        <title>{siteTitle}</title>
         <style>
           @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap');
         </style>
       </Head>
       <HomePage></HomePage>
+
+      <section >
+        <h2 >Blog</h2>
+        <ul >
+          {allPostsData.map(({ id, date, title }) => (
+            <li key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
